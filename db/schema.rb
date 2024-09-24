4b7d4f975c7071ea5bfe6d83432b4099a4cce727
marsvx <78442988+marsvx@users.noreply.github.com>
2024-09-24 13:33:51 -0600

    fixed_missspelling

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_24_173119) do
     t.string "School_email"
     t.string "major"
     t.string "minor"
-    t.date "graduuation_date"
+    t.date "graduation_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
